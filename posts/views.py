45b6d4b1ad1b64b56462d820f701e1342f494663
Rian_Prates <rianprates894@gmail.com>
2025-08-27 15:47:50 -0300

    Adição de CBV e FBV no app 'posts'

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import Post
+from django.views.generic import ListView
 
-# Create your views here.
+# FBV
+def lista_posts(request):
+    posts = Post.objects.all()
+    return render(request, "posts/lista_posts.html", {"posts": posts})
+
+# CBV
+class PostListView(ListView):
+    model = Post
+    template_name = "posts/lista_posts.html"
+    context_object_name = "posts"
