7b4852099b27e3a3cdcc8968b86ae08d6a8a9e12
Rian_Prates <rianprates894@gmail.com>
2025-08-27 14:52:34 -0300

    Funções CBV e FBV

--- heroes/views.py
+++ heroes/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 from .models import Hero
 
+
+#FBV
 def lista_herois(request):
     herois = Hero.objects.all()  # busca todos os heróis do banco
     return render(request, "heroes/lista_herois.html", {"herois": herois})
+
+#CBV 
+class HeroListView(ListView):
+    model = Hero
+    template_name = "heroes/lista_herois.html"
+    context_object_name = "herois"
